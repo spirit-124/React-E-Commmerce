ae10117b970afc9a8cc9e77fdac66cce389a5424
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-01-07 21:40:09 +0530

    [added] footer

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,36 @@
+import React from "react";
 import "./Newsletter.scss";
+import {
+  FaInstagram,
+  FaLinkedinIn,
+  FaFacebookF,
+  FaTwitter,
+} from "react-icons/fa";
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest updates and offers.</span>
+        <div className="form">
+          <input type="text" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with Privacy Policy.
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebookF />
+            <FaInstagram />
+            <FaLinkedinIn />
+            <FaTwitter />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
