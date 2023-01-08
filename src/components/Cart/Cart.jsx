e59ee7e6741ea50ebff76420f5e0e5811501aafb
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-01-08 15:47:43 +0530

    [initialized] cart section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,22 @@
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <div className="heading">Shopping Cart</div>
+          <div className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">close </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,7 @@ import Context from "../../utils/context";
 import "./Header.scss";
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -26,24 +27,27 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">VoidStores.</div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center">VoidStores.</div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={() => setShowCart(true)}>
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+    </>
   );
 };
 
