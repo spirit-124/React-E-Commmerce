962838d19e7a40c05f8b74c4c0c16511ea5ac2e2
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-01-09 21:44:54 +0530

    [completed] cart section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import { MdClose } from "react-icons/md";
-import { BsCartX } from "react-icons/bs";
+import { BsCart, BsCartX } from "react-icons/bs";
+import cartItem from "./CartItem/CartItem";
 
 import "./Cart.scss";
 const Cart = ({ setShowCart }) => {
@@ -14,6 +15,24 @@ const Cart = ({ setShowCart }) => {
             <span className="text">close </span>
           </div>
         </div>
+
+        {/* <div className="empty-cart">
+          <BsCartX />
+          <span>No products in cart.</span>
+          <button className="return-cta">Return to shop</button>
+        </div> */}
+        <>
+          <cartItem />
+          <div className="cart-footer">
+            <div className="subtotal">
+              <span className="text">Subtotal:</span>
+              <span className="text total">&#8377;1234</span>
+            </div>
+            <div className="button">
+              <button className="checkout-cta">Checkout</button>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   );
