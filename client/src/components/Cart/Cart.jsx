4c673cbf78565c4f6aacb8db8be44158b8c99c4d
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-01-23 20:17:20 +0530

    [added] stripe

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,38 +1,72 @@
+import React, { useContext } from "react";
 import { MdClose } from "react-icons/md";
-import { BsCart, BsCartX } from "react-icons/bs";
+import { BsCartX } from "react-icons/bs";
+import { Context } from "../../utils/context";
 import CartItem from "./CartItem/CartItem";
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from "../../utils/api";
 
 import "./Cart.scss";
-const Cart = ({ setShowCart }) => {
+
+const Cart = () => {
+  const { cartItems, setShowCart, cartSubTotal } = useContext(Context);
+
+  const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="cart-panel">
-      <div className="opac-layer"></div>
+      <div className="opac-layer" onClick={() => setShowCart(false)}></div>
       <div className="cart-content">
         <div className="cart-header">
-          <div className="heading">Shopping Cart</div>
-          <div className="close-btn" onClick={() => setShowCart(false)}>
-            <MdClose />
-            <span className="text">close </span>
-          </div>
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose className="close-btn" />
+            <span className="text">close</span>
+          </span>
         </div>
 
-        {/* <div className="empty-cart">
-          <BsCartX />
-          <span>No products in cart.</span>
-          <button className="return-cta">Return to shop</button>
-        </div> */}
-        <>
-          <CartItem />
-          <div className="cart-footer">
-            <div className="subtotal">
-              <span className="text">Subtotal:</span>
-              <span className="text total">&#8377;1234</span>
-            </div>
-            <div className="button">
-              <button className="checkout-cta">Checkout</button>
-            </div>
+        {!cartItems.length && (
+          <div className="empty-cart">
+            <BsCartX />
+            <span>No products in the cart.</span>
+            <button className="return-cta" onClick={() => {}}>
+              RETURN TO SHOP
+            </button>
           </div>
-        </>
+        )}
+
+        {!!cartItems.length && (
+          <>
+            <CartItem />
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button className="checkout-cta" onClick={handlePayment}>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
